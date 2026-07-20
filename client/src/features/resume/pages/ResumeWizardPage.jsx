46aacc7447071f3d61eee2e96cfb455b810e384c
avyaanverma <avyaanver@gmail.com>
2026-07-20 13:35:14 +0530

    refactor(resume): remove old wizard

--- client/src/features/resume/pages/ResumeWizardPage.jsx
+++ client/src/features/resume/pages/ResumeWizardPage.jsx
@@ -1,81 +1,9 @@
-import { useState } from "react";
+import React from 'react'
 
-import StepProgress from "../components/StepProgress/StepProgress";
-import StepContent from "../components/StepContent/StepContent";
-import WizardFooter from "../components/WizardFooter/WizardFooter";
-
-import "./styles/ResumeWizardPage.css";
-
-export default function ResumeWizardPage(){
-
-    const [currentStep,setCurrentStep] = useState(0);
-
-    const steps = [
-
-        "Profile",
-
-        "Education",
-
-        "Experience",
-
-        "Projects",
-
-        "Skills",
-
-        "Achievements",
-
-        "Certifications"
-
-    ];
-
-    function next(){
-
-        if(currentStep===steps.length-1) return;
-
-        setCurrentStep(currentStep+1);
-
-    }
-
-    function previous(){
-
-        if(currentStep===0) return;
-
-        setCurrentStep(currentStep-1);
-
-    }
-
-    return(
-
-        <main className="wizard">
-
-            <div className="wizard-body">
-
-                <StepProgress
-                    currentStep={currentStep}
-                    steps={steps}
-                />
-
-                <StepContent
-                    currentStep={currentStep}
-                    title={steps[currentStep]}
-                />
-
-                <WizardFooter
-
-                    currentStep={currentStep}
-
-                    totalSteps={steps.length}
-
-                    onNext={next}
-
-                    onPrevious={previous}
-
-                />
-
-            </div>
-
-        </main>
-
-    )
-
+const ResumeWizardPage = () => {
+  return (
+    <div>ResumeWizardPage</div>
+  )
 }
+
+export default ResumeWizardPage
