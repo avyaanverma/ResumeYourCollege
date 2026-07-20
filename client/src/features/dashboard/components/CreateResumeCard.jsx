c722a88a92ceb52707bd64b4c0dbf38bf71829b3
avyaanverma <avyaanver@gmail.com>
2026-07-20 08:54:46 +0530

    Update CreateResumeCard.jsx

--- client/src/features/dashboard/components/CreateResumeCard.jsx
+++ client/src/features/dashboard/components/CreateResumeCard.jsx
@@ -1,41 +1,32 @@
 import "./styles/CreateResumeCard.css";
 
-export default function CreateResumeCard(){
-
-    return(
-
+export default function CreateResumeCard({ onCreateResume, loading }) {
+    return (
         <section className="resume-card">
 
             <div>
-
                 <p className="resume-card-tag">
-
                     Resume Workspace
-
                 </p>
 
                 <h2>
-
                     Let's create your first resume.
-
                 </h2>
 
                 <p>
-
-                    We'll guide you through every section—from your profile and education to projects, skills, and achievements. It only takes a few minutes to get started.
-
+                    We'll guide you through every section—from your profile and
+                    education to projects, skills, and achievements.
                 </p>
-
             </div>
 
-            <button className="create-button">
-
-                Create Resume →
-
+            <button
+                className="create-button"
+                onClick={onCreateResume}
+                disabled={loading}
+            >
+                {loading ? "Creating..." : "Create Resume →"}
             </button>
 
         </section>
-
-    )
-
+    );
 }
