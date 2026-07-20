b93e28b608bc059d1d58b1e7be76547d5eefad3b
avyaanverma <avyaanver@gmail.com>
2026-07-20 08:51:03 +0530

    heroSection component added

---
+++ client/src/features/dashboard/components/HeroSection.jsx
@@ -0,0 +1,33 @@
+import "./styles/HeroSection.css";
+
+export default function HeroSection({ firstName }) {
+
+    return (
+
+        <section className="hero">
+
+            <p className="hero-tag">
+
+                Resume Builder
+
+            </p>
+
+            <h1>
+
+                Welcome back,
+                <span> {firstName}</span>
+
+            </h1>
+
+            <p>
+
+                Build a professional ATS-friendly resume section by section.
+                Your progress is automatically saved so you can continue anytime.
+
+            </p>
+
+        </section>
+
+    );
+
+}
