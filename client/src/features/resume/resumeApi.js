805df8f266feb77108ee1e11718ce2b58d99855d
avyaanverma <avyaanver@gmail.com>
2026-07-20 13:33:43 +0530

    feat(resume): add API state management

--- client/src/features/resume/resumeApi.js
+++ client/src/features/resume/resumeApi.js
@@ -1,9 +1,11 @@
 import api from "../../shared/api/http";
 
-export async function createResume() {
+const basePath = "/private/resumes";
 
-    const { data } = await api.post("/resume");
-
-    return data;
-
-}
+export const createResume = async (payload = {}) => (await api.post(basePath, payload)).data.data;
+export const getResumes = async () => (await api.get(basePath)).data.data;
+export const getResume = async (id) => (await api.get(`${basePath}/${id}`)).data.data;
+export const updateResumeSection = async (id, section, data) =>
+  (await api.patch(`${basePath}/${id}`, { section, data })).data.data;
+export const downloadResumePdf = async (id) =>
+  api.get(`${basePath}/${id}/export/pdf`, { responseType: "blob" });
---
+++ client/src/features/resume/resumeSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const resumeSlice = createSlice({
+  name: "resume",
+  initialState: { current: null },
+  reducers: {
+    setCurrentResume: (state, action) => { state.current = action.payload; },
+    clearCurrentResume: (state) => { state.current = null; },
+  },
+});
+
+export const { setCurrentResume, clearCurrentResume } = resumeSlice.actions;
+export default resumeSlice.reducer;
