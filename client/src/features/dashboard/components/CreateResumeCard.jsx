910b11296df316a1e5da720be2ed0e6821f464ca
avyaanverma <avyaanver@gmail.com>
2026-07-20 08:50:36 +0530

    createResumeSection component added

---
+++ client/src/features/dashboard/components/CreateResumeCard.jsx
@@ -0,0 +1,41 @@
+import "./styles/CreateResumeCard.css";
+
+export default function CreateResumeCard(){
+
+    return(
+
+        <section className="resume-card">
+
+            <div>
+
+                <p className="resume-card-tag">
+
+                    Resume Workspace
+
+                </p>
+
+                <h2>
+
+                    Let's create your first resume.
+
+                </h2>
+
+                <p>
+
+                    We'll guide you through every section—from your profile and education to projects, skills, and achievements. It only takes a few minutes to get started.
+
+                </p>
+
+            </div>
+
+            <button className="create-button">
+
+                Create Resume →
+
+            </button>
+
+        </section>
+
+    )
+
+}
