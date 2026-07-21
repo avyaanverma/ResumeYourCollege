d387144959fd98ba73b0bb3d807be1c4099bfaf8
avyaanverma <avyaanver@gmail.com>
2026-07-21 10:26:57 +0530

    Navbar updates for redux

--- client/src/features/dashboard/components/Navbar.jsx
+++ client/src/features/dashboard/components/Navbar.jsx
@@ -1,9 +1,14 @@
+import { useDispatch } from "react-redux";
 import "./styles/Navbar.css";
+import { clearSession } from "../../auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar({ onLogout }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   async function handleLogout() {
     try {
-      await logout();
+      await onLogout();
     } finally {
       dispatch(clearSession());
       navigate("/login");
@@ -13,10 +18,10 @@ export default function Navbar({ onLogout }) {
     <header className="dashboard-header">
       <div className="dashboard-logo">
         ResumeYour
-        <span>College</span>
+        <span>College!!</span>
       </div>
 
-      <button className="logout-button" onClick={onLogout}>
+      <button className="logout-button" onClick={handleLogout}>
         Logout
       </button>
     </header>
